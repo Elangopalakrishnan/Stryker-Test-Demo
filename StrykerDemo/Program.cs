3920bfddc30619a395b81792b5b7b51a91ba3e29
pree <epkmca@gmail.com>
2022-05-15 10:54:40 +0100

    Initial commit

---
+++ StrykerDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StrykerDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's create some mutant!");
+        }
+
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int product(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int division(int x, int y)
+        {
+            return x / y;
+        }
+
+        public int mod(int x, int y)
+        {
+            return x % y;
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using StrykerDemo;
+
+namespace UnitTest
+{
+    public class UnitTest1 : Program
+    {
+        [Fact]
+        public void AdditionTest()
+        {
+            Assert.Equal(10, add(5, 5));
+        }
+
+        [Fact]
+        public void SubtractionTest()
+        {
+            Assert.Equal(10, subtract(15, 5));
+        }
+
+        [Fact]
+        public void ModularTest()
+        {
+            Assert.Equal(3, mod(15, 4));
+        }
+    }
+}
